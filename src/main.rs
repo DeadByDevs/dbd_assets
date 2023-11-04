79bb54138673edce44a1f0963d4d8d8d6472e74d
yassin <yassinam.pro@gmail.com>
2023-11-04 12:11:56 +0100

    :memo: [FIX] created function that creates folder assets if not exist to avoi no such file or directory

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use std::io::Write;
 mod config;
 use crate::config::DBD_OFFICIAL_API;
 
+fn create_folder_if_not_exist() {
+    let assets_folder: &str = "./assets";
+    if !std::path::Path::new(assets_folder).exists() {
+        std::fs::create_dir(assets_folder).unwrap();
+    }
+}
+
 async fn download_from_url(url: &str, save_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::get(url).await?;
     let mut file = File::create(save_path)?;
@@ -12,13 +19,13 @@ async fn download_from_url(url: &str, save_path: &str) -> Result<(), Box<dyn std
 }
 
 async fn download() {
-    let maps_url =  DBD_OFFICIAL_API.to_owned() + "maps/page-data.json";
-    let characters_url = DBD_OFFICIAL_API.to_owned() + "characters/page-data.json";
-    let chapters_url = DBD_OFFICIAL_API.to_owned() + "chapters/page-data.json";
+    let maps_url: String = DBD_OFFICIAL_API.to_owned() + "maps/page-data.json";
+    let characters_url: String = DBD_OFFICIAL_API.to_owned() + "characters/page-data.json";
+    let chapters_url: String = DBD_OFFICIAL_API.to_owned() + "chapters/page-data.json";
 
-    let maps_save_path = "./assets/maps.json";
-    let characters_save_path = "./assets/characters.json";
-    let chapters_save_path = "./assets/chapters.json";
+    let maps_save_path: &str = "./assets/maps.json";
+    let characters_save_path: &str = "./assets/characters.json";
+    let chapters_save_path: &str = "./assets/chapters.json";
 
     if let Err(err) = download_from_url(&maps_url, maps_save_path).await {
         eprintln!("Failed to download maps: {}", err);
@@ -39,10 +46,9 @@ async fn download() {
     }
 }
 
-
 fn main() {
+    create_folder_if_not_exist();
     println!("Downloading assets...");
     let runtime = tokio::runtime::Runtime::new().unwrap();
     runtime.block_on(download());
 }
-
